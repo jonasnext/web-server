b22f82acfd9bd9fee1448b790050a6b59436a18c
jonas <jonasnext@users.noreply.github.com>
2020-12-25 12:26:58 +0100

    simple web/http server

--- main.go
+++ main.go
@@ -1,17 +1,9 @@
-// https://golang.org/doc/articles/wiki/
-// https://golang.org/pkg/net/http
-
 package main
 
 import (
-    "fmt"
     "log"
     "net/http"
 )
-
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "👋 🙌 %s", r.URL.Path[1:])
-}
 
 func main() {
     log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("."))))
